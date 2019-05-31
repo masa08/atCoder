d82bde21f3acf89cf35767e9e6552a43d8527282
Masa08 <ushimasarug08@gmail.com>
2019-05-31 10:14:24 +0900

    update 110B

--- 110/b.rb
+++ 110/b.rb
@@ -3,11 +3,6 @@ number_x = gets.split.map(&:to_i)
 number_y = gets.split.map(&:to_i)
 number_x << x
 number_y << y
-# z = [*x+1..y]
-# r = []
-# z.each do |z|
-#   r << z if z > number_x.max && z <= number_y.min
-# end
 puts number_x.max >= number_y.min ? "War" : "No War"
 
 # r =[]
